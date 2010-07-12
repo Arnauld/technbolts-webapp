483feadf743f2130dce18f4b5c2fb33d10dbfaed
Arnauld <arnauld.loyer@gmail.com>
2010-07-12 16:53:43 +0200

    add User support
    add h2 support
    
    add user in log when logged in

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -6,19 +6,77 @@ import _root_.net.liftweb.http._
 import _root_.net.liftweb.sitemap._
 import _root_.net.liftweb.sitemap.Loc._
 import Helpers._
+import org.slf4j.{Logger, LoggerFactory}
+import org.technbolts.model.User
+import provider.HTTPRequest
+import net.liftweb.mapper.{Schemifier, DefaultConnectionIdentifier, StandardDBVendor, DB}
 
 /**
  * A class that's instantiated early and run.  It allows the application
  * to modify lift's environment
  */
 class Boot {
+  private val logger: Logger = LoggerFactory.getLogger(classOf[Boot])
+
   def boot {
+
+    if (!DB.jndiJdbcConnAvailable_?) {
+      val vendor = new StandardDBVendor(
+        Props.get("db.driver") openOr "org.h2.Driver", //
+        Props.get("db.url") openOr "jdbc:h2:db/technbolts.db;AUTO_SERVER=TRUE", //
+        Props.get("db.user"), //
+        Props.get("db.password"))
+
+      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
+      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
+    }
+
     // where to search snippet
     LiftRules.addToPackages("org.technbolts")
 
+    Schemifier.schemify(true, Schemifier.infoF _, User)
+
+    LiftSession.onSetupSession = List((s: LiftSession) => {
+      logger.info("Session setup: " + s)
+    })
+
+    S.addAround(new LoanWrapper { // Y
+      def apply[T](f: => T): T = {
+        User.currentUser match {
+          case Full(user) => MDC.put(("user", user.email))
+          case _ => MDC.put(("user", "anonymous"))
+        }
+        val result = f // Let Lift do normal request processing.
+        result
+      }
+    })
+
     // Build SiteMap
-    val entries = Menu(Loc("Home", List("index"), "Home")) :: Nil
+    val entries = Menu(Loc("Home", List("index"), "Home")) :: User.sitemap
     LiftRules.setSiteMap(SiteMap(entries: _*))
+
+    /*
+     * Show the spinny image when an Ajax call starts
+     */
+    LiftRules.ajaxStart = Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+
+    /*
+     * Make the spinny image go away when it ends
+     */
+    LiftRules.ajaxEnd = Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+
+    /*
+     * Force the request to be UTF-8
+     */
+    LiftRules.early.append(makeUtf8)
   }
+
+  /**
+   * Force the request to be UTF-8
+   */
+  private def makeUtf8(req: HTTPRequest) {
+    req.setCharacterEncoding("UTF-8")
+  }
+
 }
 
---
+++ src/main/scala/org/technbolts/model/User.scala
@@ -0,0 +1,45 @@
+package org.technbolts.model
+
+// Import all of the mapper classes
+import _root_.net.liftweb.mapper._
+
+// Create a User class extending the Mapper base class
+// MegaProtoUser, which provides default fields and methods
+// for a site user.
+class User extends MegaProtoUser[User] {
+  def getSingleton = User // reference to the companion object below
+
+  // define an additional field for a personal essay
+  object textArea extends MappedTextarea(this, 2048) {
+    override def textareaRows  = 10
+    override def textareaCols = 50
+    override def displayName = "Personal Essay"
+  }
+}
+
+// Create a "companion object" to the User class (above).
+// The companion object is a "singleton" object that shares the same
+// name as its companion class. It provides global (i.e. non-instance)
+// methods and fields, such as find, dbTableName, dbIndexes, etc.
+// For more, see the Scala documentation on singleton objects
+object User extends User with MetaMegaProtoUser[User] {
+  override def dbTableName = "users" // define the DB table name
+
+  // define the order fields will appear in forms and output
+  override def fieldOrder = List(id, firstName, lastName, email, locale, timezone, password, textArea)
+
+  // comment this line out to require email validations
+  override def skipEmailValidation = true
+
+  // Provide our own login page template.
+  override def loginXhtml =
+    <lift:surround with="default" at="content">
+      {super.loginXhtml}
+    </lift:surround>
+
+  // Provide our own signup page template.
+  override def signupXhtml(user: User) =
+    <lift:surround with="default" at="content">
+      {super.signupXhtml(user)}
+    </lift:surround>
+}
