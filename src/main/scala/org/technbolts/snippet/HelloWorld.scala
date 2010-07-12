0d5a1aa2e1cc61e0a5219712aa24df11f3898949
Arnauld <arnauld.loyer@gmail.com>
2010-07-12 17:13:04 +0200

    Add user in welcome page

--- src/main/scala/org/technbolts/snippet/HelloWorld.scala
+++ src/main/scala/org/technbolts/snippet/HelloWorld.scala
@@ -1,11 +1,15 @@
 package org.technbolts.snippet
 
 import _root_.scala.xml.NodeSeq
+
+import _root_.net.liftweb.common._
+import _root_.net.liftweb.util._
 import _root_.net.liftweb.util.Helpers
 import Helpers._
 import org.springframework.beans.factory.annotation.{Qualifier, Autowired, Configurable}
 import org.technbolts.util.TimeService
 import org.slf4j.{Logger, LoggerFactory}
+import org.technbolts.model.User
 
 @Configurable
 class HelloWorld {
@@ -20,6 +24,10 @@ class HelloWorld {
 
   def howdy(in: NodeSeq): NodeSeq = {
     logger.debug("howdy !");
-    Helpers.bind("b", in, "time" -> timeService.getFormattedTime)
+    val user:String = User.currentUser match {
+      case Full(user) => user.email
+      case _ => "anonymous"
+    }
+    Helpers.bind("b", in, "time" -> timeService.getFormattedTime, "user" -> user)
   }
 }
